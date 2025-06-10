451fa9303abd20ff3822a6a3f68b066a62eac204
Arun Somasekharan <arunsomasekh@gmail.com>
2025-06-10 09:05:44 +0100

    Bash script added for copying files from windows file system

---
+++ cpfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp /mnt/c/Users/Public/Aruns/DevAndRelatedStuff/PersonalWebsite/index.md .
+cp /mnt/c/Users/Public/Aruns/DevAndRelatedStuff/PersonalWebsite/projects.md .
+cp /mnt/c/Users/Public/Aruns/DevAndRelatedStuff/PersonalWebsite/cv.md .
+cp /mnt/c/Users/Public/Aruns/DevAndRelatedStuff/PersonalWebsite/_config.yml .
+cp /mnt/c/Users/Public/Aruns/DevAndRelatedStuff/PersonalWebsite/_layouts/default.html _layouts/
+cp /mnt/c/Users/Public/Aruns/DevAndRelatedStuff/PersonalWebsite/Gemfile .
